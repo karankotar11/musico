8d4bfa68033047542e2917b1256b572c04751908
karankotar11 <karankotar11@gmail.com>
2025-05-05 12:12:08 +0530

    Update page.tsx set pass for auth

--- src/app/delete_song/page.tsx
+++ src/app/delete_song/page.tsx
@@ -27,6 +27,8 @@ const DeleteSongPage = () => {
     const [initialLoad, setInitialLoad] = useState(true);
 
     const { filteredMusic, handleSearch } = useMusicSearch();
+    const [authenticated, setAuthenticated] = useState(false);
+    const [password, setPassword] = useState('');
 
     useEffect(() => {
         setSongs(filteredMusic);
@@ -106,6 +108,36 @@ const DeleteSongPage = () => {
             setLoading(false);
         }
     };
+    const handlePasswordSubmit = () => {
+        if (password === 'kk10') {
+            setAuthenticated(true);
+        } else {
+            alert('Incorrect password!');
+        }
+    };
+    if (!authenticated) {
+        return (
+            <div className="p-6 max-w-sm mx-auto mt-20">
+                <form>
+                    <h2 className="text-xl font-semibold mb-4">Enter Password</h2>
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        className="w-full border p-2 mb-2"
+                    />
+                    <button
+                        onClick={handlePasswordSubmit}
+                        onSubmit={handlePasswordSubmit}
+                        className="bg-blue-600 text-white px-4 py-2 rounded"
+                    >
+                        Submit
+                    </button>
+                </form>
+            </div>
+        );
+    }
 
     return (
         <>
